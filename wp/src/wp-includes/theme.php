ab5c1fe09a20806ad0b03509f0257a8f2f53652a
serhii <serhiicho@protonmail.com>
2020-08-25 16:19:04 +0300

    Changed submodule reference

--- wp/src/wp-includes/theme.php
+++ wp/src/wp-includes/theme.php
@@ -2242,7 +2242,7 @@ function add_theme_support( $feature ) {
 			 * Merge post types with any that already declared their support
 			 * for post thumbnails.
 			 */
-			if ( is_array( $args[0] ) && isset( $_wp_theme_features['post-thumbnails'] ) ) {
+			if ( is_array($args) && is_array( $args[0] ) && isset( $_wp_theme_features['post-thumbnails'] ) ) {
 				$args[0] = array_unique( array_merge( $_wp_theme_features['post-thumbnails'][0], $args[0] ) );
 			}
 
